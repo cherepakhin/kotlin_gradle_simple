1857433f69a252b215283d18e9e0494d79c29b53
Черепахин Василий <vasi.che@gmail.com>
2025-01-08 12:27:47 +0500

    add ProductServiceImplTest.getByN()

---
+++ src/test/kotlin/ru/perm/v/shopkotlin/service/impl/ProductServiceImplTest.kt
@@ -0,0 +1,12 @@
+package ru.perm.v.shopkotlin.service.impl
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ProductServiceImplTest {
+
+    @Test
+    fun getByN() {
+    }
+}
